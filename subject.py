7e4b46362870a8f57bb68a2e9983931e48f4a2fa
Manuel Andrade <104844085+ManuelAndrade1@users.noreply.github.com>
2023-07-23 16:36:21 -0300

    Re-structured Subject's attributes. Added some constructor functions and re-made the tests for said attributes.

--- subject.py
+++ subject.py
@@ -1,16 +1,44 @@
 #!python3
 
+from typing import List, Dict
+
+
 BLOCKS = ["8:00-9:40", "9:50-11:30", "11:40-13:20", "13:30-15:10", "15:20-17:00", "17:10-18:50"]
+
 class Subject:
-	def __init__(self, sbj:str, section:int, block:int):
+	def __init__(self, sbj:str, section:int, sched:Dict[str, List[int]]):
 		# Would be nice to add professors attr.
 		self.subject:str = sbj
 		self.section:int = section
-		self.block:int = block
-		self.time:str = BLOCKS[block]
+		self.schedule:Dict[str, List[int]] = sched
+		self.time_schedule:Dict[str, List[str]] = format_schedule(self.schedule)
 
 	def __repr__(self):
-		return self.subject + ", " + str(self.section) + ", " + self.time 
-
+		vr:str = f"""{self.subject}, {str(self.section)}, {str(list((self.schedule.keys())))}"""
+		return vr
 	def __eq__(self, other):
-		return self.subject == other.subject and self.block == other.block
+		return self.subject == other.subject and self.schedule == other.schedule
+
+def format_schedule(schedule:Dict[str, List[int]]) -> Dict[str, List[str]]:
+	"""
+	Requires: Schedule dictionary with days of the week as keys and list with 
+	time-blocks as values (0-5)
+	Returns: Dict containing days as keys and list of string time-blocks as values
+	"""
+	vr:Dict[str, List[str]] = {}
+
+	for k, v in schedule.items():
+		vr[k] = time_blocks(v)
+
+	return vr
+
+def time_blocks(block_list:List[int]) -> List[str]:
+	"""
+	Requires: List of integers from 0-5
+	Returns: Equivalent time-blocks in string format
+	"""
+	vr:List[str] = []
+	for i in block_list:
+		vr.append(BLOCKS[i])
+
+	return vr
--- subject_test.py
+++ subject_test.py
@@ -3,9 +3,27 @@
 import unittest
 from subject import Subject
 
+TEST1:Subject = Subject("Matematica II", 6, {"Lun": [0, 1], "Mierc":[0, 1]})
+TEST2:Subject = Subject("Matematica II", 8, {"Mierc":[0, 1], "Lun":[0, 1]})
+TEST3:Subject = Subject("Matematica II", 10, {"Mar":[0, 1], "Juev":[0, 1]})
+
 class TestSubject(unittest.TestCase):
+	def test_attrs(self):
+		self.assertEqual(TEST1.subject, "Matematica II")
+		self.assertEqual(TEST1.section, 6)
+		self.assertEqual(TEST1.schedule, {"Mierc":[0, 1], "Lun":[0, 1]})
+		self.assertEqual(TEST1.time_schedule, 
+			{"Mierc":["8:00-9:40", "9:50-11:30"], "Lun":["8:00-9:40", "9:50-11:30"]})
+
 	def test_repr(self):
-		test1:Subject = Subject("Matematica II", 6, 0)
-		self.assertEqual(str(test1), "Matematica II, 6, 8:00-9:40")
+		exp_text:str = "Matematica II, 6, ['Lun', 'Mierc']"
+		exp_text2:str = "Matematica II, 6, ['Mierc', 'Lun']"
+		self.assertTrue(str(TEST1) == exp_text or str(TEST1) == exp_text2)
+
+	def test_eq(self):
+		self.assertTrue(TEST1 == TEST2)
+		self.assertFalse(TEST1 == TEST3)
+		
+unittest.main()
 
 
